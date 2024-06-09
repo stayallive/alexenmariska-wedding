411a126bec738be16ae07807f8f09134ab365b66
Alex Bouma <alex@bouma.me>
2024-06-09 14:15:58 +0200

    Fix input zoom

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html class="h-full" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="robots" content="noindex">
 
         <title>Alex & Mariska Wedding</title>
