48224772c067eeed4936d7e715d568e135add937
Alex Bouma <alex@bouma.me>
2024-06-23 21:04:51 +0200

    Try more scroll issue fixes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -19,7 +19,7 @@
         <style>{!! file_get_contents(resource_path('css/fonts.css')) !!}</style>
     </head>
     <body class="text-neutral-700">
-        <div class="h-screen w-screen bg-cover bg-no-repeat bg-center" style="background-image: url('{{ asset('images/background.jpg') }}')">
+        <div class="h-dvh w-full bg-cover bg-no-repeat bg-center" style="background-image: url('{{ asset('images/background.jpg') }}')">
             @yield('content')
         </div>
 
