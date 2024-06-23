1d9bb6913dcc6c3952fa640579721d3fd30e9f9c
Alex Bouma <alex@bouma.me>
2024-06-23 21:18:49 +0200

    wip

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -4,7 +4,7 @@
 
 <div class="h-dvh w-screen overflow-y-scroll">
     <div class="min-h-dvh flex items-center">
-        <div class="mx-auto p-4 sm:py-4 sm:w-96">
+        <div class="mx-auto p-4 min-w-screen sm:py-4 sm:w-96">
             <div class="min-h-60 flex flex-col bg-white border shadow-sm rounded-card w-full">
                 <div class="flex flex-auto flex-col justify-center items-center px-4 py-6">
                     <h3 class="text-5xl leading-[4rem] font-['Braveheart'] text-copper">
